4a731726b6a841ab12db3b4a4067bf2b96bf67a2
Thunderkill <thunderkill@live.fi>
2021-10-28 09:05:33 +0300

    asdsad

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 
-listenAndServe(":80", () => new Response("Hello World v5!\n"));
+listenAndServe(":80", () => new Response("Hello World v6!\n"));
 
 console.log("Started simple web server");
